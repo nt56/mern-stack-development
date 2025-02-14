797df3f14491a41f4cef099150c3de5602b89574
nt_56 <nagutirth2121@gmail.com>
2025-02-14 18:36:13 +0530

    react class-3 added (folder structure and angular)

---
+++ REACT/Class-3/code/basic-app/src/index.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div>
+      <h2>Frontend Technologies</h2>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Tailwind CSS</li>
+        <li>Bootstrap</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Angular</li>
+        <li>View</li>
+      </ul>
+      <br></br>
+      <h2>Backend Technologies</h2>
+      <ul>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>SQL</li>
+        <li>JavaScript</li>
+        <li>Postman</li>
+        <li>Springboot</li>
+        <li>Hibernet</li>
+      </ul>
+      <br></br>
+      <h2>Full Stack Technologies</h2>
+      <ul>
+        <li>Node</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+        <li>SQL</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>HTML</li>
+        <li>CSS</li>
+      </ul>
+    </div>
+  </React.StrictMode>
+);
