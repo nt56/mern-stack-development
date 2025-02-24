00892dc076b3991c9ac142b15c33023c2990ba44
nt_56 <nagutirth2121@gmail.com>
2025-02-24 14:20:24 +0530

    class-9 react code added && small project

--- REACT/Class-8/code/src/App.jsx
+++ REACT/Class-8/code/src/App.jsx
@@ -4,10 +4,32 @@ import Product from "./Product";
 class App extends React.Component {
   constructor() {
     super();
+
+    this.state = {
+      food: false,
+      electronics: false,
+      other: false,
+    };
   }
 
+  handleFoodProducts = () => {
+    this.setState({ food: true, electronics: false, other: false });
+  };
+
+  handleElectronicsProducts = () => {
+    this.setState({ food: false, electronics: true, other: false });
+  };
+
+  handleOtherProducts = () => {
+    this.setState({ food: false, electronics: false, other: true });
+  };
+
+  handleAllProducts = () => {
+    this.setState({ food: true, electronics: true, other: true });
+  };
+
   render() {
-    const productLists = [
+    const foodProducts = [
       {
         id: 1,
         name: "KFC ",
@@ -24,57 +46,119 @@ class App extends React.Component {
       },
       {
         id: 3,
+        name: "Pizza",
+        img: "https://www.yum.com/wps/wcm/connect/yumbrands/77ac5d27-1357-4792-9953-54b11f5ae7dd/yum-com-24-product-PH.jpg?MOD=AJPERES&CACHEID=ROOTWORKSPACE.Z18_5QC4HBC039RJ406SQH4UBH3695-77ac5d27-1357-4792-9953-54b11f5ae7dd-oXSxcXb",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 4,
+        name: "Chicken",
+        img: "https://assets.epicurious.com/photos/663552459a068917f23d1e92/4:3/w_3830,h_2872,c_limit/cornell-bbq-chicken_RECIPE_042524_5318_VOG_final.jpg",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 5,
+        name: "Paneer",
+        img: "https://www.eitanbernath.com/wp-content/uploads/2020/05/Butter-Paneer-1-4x5-LOW-RES-819x1024.jpeg",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 6,
+        name: "Egg",
+        img: "https://www.licious.in/blog/wp-content/uploads/2022/03/shutterstock_245119171-min-1.jpg",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+    ];
+
+    const electronicsProducts = [
+      {
+        id: 1,
         name: "Samsung s24",
         img: "https://m.media-amazon.com/images/I/71uqj6BKnRL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 4,
+        id: 2,
         name: "Iphone 16",
         img: "https://m.media-amazon.com/images/I/619oqSJVY5L._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 5,
+        id: 3,
         name: "Fastrack Watch",
         img: "https://m.media-amazon.com/images/I/61rbYFj7DgL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 6,
+        id: 4,
         name: "Boat Watch",
         img: "https://m.media-amazon.com/images/I/61Fwv0TtQnL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 7,
+        id: 5,
         name: "Boat Speaker",
         img: "https://m.media-amazon.com/images/I/71-I9Bk8dgL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 8,
+        id: 6,
         name: "JBL Speaker",
         img: "https://m.media-amazon.com/images/I/51waOv47fqL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
       {
-        id: 9,
+        id: 7,
         name: "Boat HeadSet",
         img: "https://m.media-amazon.com/images/I/51FNnHjzhQL._AC_UY218_.jpg",
         btn1: "Add",
         btn2: "Buy Now",
       },
+    ];
+
+    const otherProducts = [
       {
-        id: 10,
+        id: 1,
         name: "Campus Shoes",
         img: "https://m.media-amazon.com/images/I/61CcXNL-ANL._AC_UL320_.jpg",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 2,
+        name: "T-Shirt",
+        img: "https://rukminim2.flixcart.com/image/612/612/xif0q/t-shirt/o/x/u/l-tblhn-dp1-d144-tripr-original-imah888zbpdmepmq.jpeg?q=70",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 3,
+        name: "Bottol",
+        img: "https://rukminim2.flixcart.com/image/612/612/xif0q/shopsy-bottle/j/k/0/720-new-insulated-bottle-for-school-collage-and-travelling-1-original-imah7uzzrn88ubhj.jpeg?q=70",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 4,
+        name: "Book",
+        img: "https://rukminim2.flixcart.com/image/612/612/xif0q/book/c/t/f/man-s-search-for-meaning-original-imah9bc2mvb8hnw6.jpeg?q=70",
+        btn1: "Add",
+        btn2: "Buy Now",
+      },
+      {
+        id: 5,
+        name: "Sunglass",
+        img: "https://rukminim2.flixcart.com/image/612/612/xif0q/sunglass/j/q/x/p429gr3v-one-size-fits-all-fastrack-original-imagp3eusz94znhy.jpeg?q=70",
         btn1: "Add",
         btn2: "Buy Now",
       },
@@ -89,17 +173,67 @@ class App extends React.Component {
             </div>
           </div>
 
+          <div className="d-flex justify-content-center gap-5">
+            <button
+              className="btn btn-primary fw-bold"
+              onClick={() => this.handleAllProducts()}
+            >
+              All Products
+            </button>
+            <button
+              className="btn btn-primary fw-bold"
+              onClick={() => this.handleFoodProducts()}
+            >
+              Food Products
+            </button>
+            <button
+              className="btn btn-primary fw-bold"
+              onClick={() => this.handleElectronicsProducts()}
+            >
+              Electronic Products
+            </button>
+            <button
+              className="btn btn-primary fw-bold"
+              onClick={() => this.handleOtherProducts()}
+            >
+              Other Products
+            </button>
+          </div>
+
           <div className="col-md-12 bg-light-subtle object-fit-contain">
             <div className="row justify-content-center">
-              {productLists.map((p) => (
-                <Product
-                  key={p.id}
-                  name={p.name}
-                  photo={p.img}
-                  btn1={p.btn1}
-                  btn2={p.btn2}
-                />
-              ))}
+              {this.state.food &&
+                foodProducts.map((p) => (
+                  <Product
+                    key={p.id}
+                    name={p.name}
+                    photo={p.img}
+                    btn1={p.btn1}
+                    btn2={p.btn2}
+                  />
+                ))}
+
+              {this.state.electronics &&
+                electronicsProducts.map((p) => (
+                  <Product
+                    key={p.id}
+                    name={p.name}
+                    photo={p.img}
+                    btn1={p.btn1}
+                    btn2={p.btn2}
+                  />
+                ))}
+
+              {this.state.other &&
+                otherProducts.map((p) => (
+                  <Product
+                    key={p.id}
+                    name={p.name}
+                    photo={p.img}
+                    btn1={p.btn1}
+                    btn2={p.btn2}
+                  />
+                ))}
             </div>
           </div>
         </div>
